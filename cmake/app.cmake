302bab9805cc59645acfcc5876b8a80c05894b8f
Adam Brzakala <adam.brz@onet.eu>
2022-10-20 19:25:27 +0200

    Rearange project structure, add presets for debug builds

---
+++ cmake/app.cmake
@@ -0,0 +1,36 @@
+function(setup_dependencies)
+    find_package(FreeGLUT REQUIRED)
+    find_package(GLEW REQUIRED)
+    find_package(glm REQUIRED)
+endfunction()
+
+function(setup_app APP_TARGET_NAME)
+    target_include_directories(${APP_TARGET_NAME}
+    PUBLIC
+        include/
+    PRIVATE
+        ${FreeGLUT_INCLUDE_DIRS}
+        ${GLEW_INCLUDE_DIRS}
+        ${glm_INCLUDE_DIRS}
+    )
+
+    target_link_libraries(${APP_TARGET_NAME}
+        FreeGLUT::freeglut_static
+        GLEW::GLEW
+        glm::glm
+    )
+
+    set_target_properties(${APP_TARGET_NAME} PROPERTIES
+        CXX_STANDARD 17
+        CXX_STANDARD_REQUIRED TRUE
+        ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+        LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+    )
+
+    if (MSVC)
+        target_compile_options(${APP_TARGET_NAME} PRIVATE /W4 $<IF:$<CONFIG:Debug>,/Zi,/O2>)
+    else()
+        target_compile_options(${APP_TARGET_NAME} PRIVATE -Wall -Wextra -pedantic $<IF:$<CONFIG:Debug>,-g,-O3>)
+    endif()
+endfunction()
--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -1,15 +1,16 @@
 set(CONAN_TOOLCHAIN_FILENAME ${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
 
-function(setup_conan_toolchain)
+macro(setup_conan_toolchain)
     if(NOT EXISTS ${CONAN_TOOLCHAIN_FILENAME})
         execute_process(
-            COMMAND conan install ${CMAKE_SOURCE_DIR}/conan -pr:b=default --build=missing -s build_type=${CMAKE_BUILD_TYPE}
+            COMMAND conan install ${CMAKE_SOURCE_DIR}/conan -pr:b=default --build=missing -s=build_type=${CMAKE_BUILD_TYPE}
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             COMMAND_ECHO STDOUT
         )
         replace_in_file(${CONAN_TOOLCHAIN_FILENAME} "set(CMAKE_GENERATOR_PLATFORM \"x64\" CACHE STRING \"\" FORCE)" "")
     endif()
-endfunction()
+    include(${CONAN_TOOLCHAIN_FILENAME})
+endmacro()
 
 function(replace_in_file FILE TEXT_TO_REPLACE TEXT)
     file(READ "${FILE}" FILE_CONTENTS)
